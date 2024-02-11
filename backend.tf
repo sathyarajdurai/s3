68090bc571738814bf6bd19bc8b5456a8868aa4f
sathyarajdurai <sathyaraj.durai@cloudreach.com>
2024-02-11 20:41:45 +0530

    new bracnch

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
     backend "s3" {
-        bucket = "talent-academy-sathyaraj-lab-tfstates1"
+        bucket = "talent-academy-sathyaraj-lab-tfstates-feb24"
         key = "talent-academy/s3/terraform.tfstates"
         dynamodb_table = "terraform-lock"
     }
